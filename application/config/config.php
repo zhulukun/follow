4abc4b477caa511105c1a85337590cbf3b28f63d
zlk <1770195221@qq.com>
2015-12-29 17:12:44 +0800

    update

--- application/config/config.php
+++ application/config/config.php
@@ -17,7 +17,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | environments.
 |
 */
-$config['base_url'] = 'http://118.187.12.67/follow/';
+$config['base_url'] = 'http://localhost/follow/';
 
 /*
 |--------------------------------------------------------------------------
